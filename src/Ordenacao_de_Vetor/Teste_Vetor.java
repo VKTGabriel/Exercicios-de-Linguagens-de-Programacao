c21b78782c4d3d598b3d28c75cf76ea4bdf6de42
VKTGabriel <victorpedrosa66@yahoo.com.br>
2024-10-24 15:37:10 -0300

    Exercicios de Linguagem de Programação

--- src/Ordenacao_de_Vetor/Teste_Vetor.java
+++ src/Ordenacao_de_Vetor/Teste_Vetor.java
@@ -4,11 +4,15 @@ public class Teste_Vetor {
     public static void main(String[] args){
 
         int[] vA = {3,7,4,5,9,64,14,-4,11,6};
-        Vetor v = new Vetor();
+        Vetor v1 = new Vetor(vA);
+        Vetor v2 = new Vetor();
 
-        v.metodoBubble();
-        v.metodoSelection();
-        v.metodoInsert();
-        v.metodoHeapfy();
+        System.out.println("\nOrdenação de v1:");
+        v1.metodoBubble();
+        v1.metodoSelection();
+
+        System.out.println("\nOrdenação de v2:");
+        v2.metodoInsert();
+        v2.metodoHeapfy();
     }
 }
